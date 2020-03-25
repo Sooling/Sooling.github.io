79739117a5177ed335c86b8de65ed0b0d011021e
Sooling <1391040917@qq.com>
2020-03-25 18:40:49 +0800

    Site updated: 2020-03-25 18:40:47

--- js/header.js
+++ js/header.js
@@ -4,6 +4,83 @@ $(document).ready(function () {
   var $menuItem = $('.header-nav-menu-item');
   var $submenu = $('.header-nav-submenu');
   var isMobile = $menuBtn.is(':visible');
+  
+(function cust_valine(){
+	  var $valinecontainer = $('#valine-container');
+		var $vheader = $valinecontainer.find('.vheader');
+		var $vinput = $valinecontainer.find('.vinput');
+	  var $vlink = $valinecontainer.find('.vlink');
+	  $vlink.remove();
+		var $vnick = $valinecontainer.find('.vnick');
+		var $vmail = $valinecontainer.find('.vmail');
+		var $vsubmit = $valinecontainer.find('.vsubmit');
+		var verif_nick = true;
+		var verif_mail = true;	
+		
+		$vheader.css('position','relative');
+		
+		var $vcue = $('<label class="vcue" style="position: absolute;color: #FF0000;font-size: 10px;padding: 0 5px"></label>');
+		$vcue.css({'top': '-10px','left': '0px'});
+				
+		$vheader.append($vcue);
+		
+		$vnick.bind('focus keyup',function(){
+			var $vnick_val = $.trim(this.value);
+			if(!$vnick_val){
+				verif_nick = true;
+				$vcue.text('提示：请输入昵称！（非必须）');
+			}else{
+				verif_nick = false;
+				$vcue.text('');				
+			};
+		});
+		$vnick.bind('blur',function(){
+			$vcue.text('');
+		});
+		
+		$vmail.bind('focus keyup',function(){
+			var $vmail_val = $.trim(this.value);
+			var reg = /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/;
+			if(!$vmail_val){
+				verif_mail = true;
+				$vcue.text('提示：请输入邮箱！（非必须）');
+			}else if(!reg.test($vmail_val)){
+				verif_mail = true;
+				$vcue.text('提示：邮箱不完整或格式不正确！');
+			}else{
+				verif_mail = false;
+				$vcue.text('');				
+			}
+		});
+		$vmail.bind('blur',function(){
+			$vcue.text('');
+		});
+		
+		/*$vsubmit.bind('click',function(){
+			if(verif_nick || verif_mail){
+				$(this).attr('disabled','disabled');
+				$vcue.text('请检查邮箱和用户名！');
+			};
+		});*/
+		/* $vsubmit.bind('mouseover',function(){
+			if(verif_nick === false && verif_mail === false){
+				$(this).removeAttr('disabled');
+			}else{
+				$vcue.text('请检查邮箱和用户名！');
+				$(this).attr('disabled','disabled');
+			};
+		});
+		
+		$vsubmit.bind('touchstart',function(){
+			if(!(verif_nick || verif_mail)){
+				$(this).removeAttr('disabled');
+			}else{
+				$vcue.text('请检查邮箱和用户名！');
+				$(this).attr('disabled','disabled');
+			};
+		}); */
+		
+})();
 
   var isMenuShow = false;
   var isSubmenuShow = false;
